e450714a5ee5f2ad09587fd7022dcb622597ac59
JayRen <monkey407ya@gmail.com>
2024-05-03 17:36:59 +0800

    Reformatting

--- src/hw4/BorrowMoney.java
+++ src/hw4/BorrowMoney.java
@@ -4,56 +4,60 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 /**
- * ClassName： BorrowMoney
- * package：hw4
- * Description：
+ * ClassName： BorrowMoney package：hw4 Description：
  *
  * @Author 任少騫
  * @Create 2024/5/1 @{TIME}
  * @Version 1.0
  */
 public class BorrowMoney {
-    public static void main(String[] args) {
-        Employees wen = new Employees();
-        Employees[] emp = new Employees[5];
-        emp[0] = new Employees();
-        emp[0].empId = 25; emp[0].money = 2500;
+	public static void main(String[] args) {
+		Employees wen = new Employees();
+		Employees[] emp = new Employees[5];
+		emp[0] = new Employees();
+		emp[0].empId = 25;
+		emp[0].money = 2500;
 
-        emp[1] = new Employees();
-        emp[1].empId = 32; emp[1].money = 800;
+		emp[1] = new Employees();
+		emp[1].empId = 32;
+		emp[1].money = 800;
 
-        emp[2] = new Employees();
-        emp[2].empId = 8;  emp[2].money = 500;
+		emp[2] = new Employees();
+		emp[2].empId = 8;
+		emp[2].money = 500;
 
-        emp[3] = new Employees();
-        emp[3].empId = 19; emp[3].money = 1000;
+		emp[3] = new Employees();
+		emp[3].empId = 19;
+		emp[3].money = 1000;
 
-        emp[4] = new Employees();
-        emp[4].empId = 27; emp[4].money = 1200;
-        wen.sift(emp);
-    }
+		emp[4] = new Employees();
+		emp[4].empId = 27;
+		emp[4].money = 1200;
+		wen.sift(emp);
+	}
 }
 
-class Employees{
-    Scanner sc = new Scanner(System.in);
-    int borrowMoney;
-    int empId;
-    int money;
-    public Employees(){
-    }
+class Employees {
+	Scanner sc = new Scanner(System.in);
+	int borrowMoney;
+	int empId;
+	int money;
 
-    public void sift(Employees[] emp){
-        System.out.println("窮鬼阿文要借多少錢：");
-        borrowMoney = sc.nextInt();
-        int count = 0;
-        System.out.print("員工編號：");
-        for (int i = 0; i < emp.length; i++) {
-            if(borrowMoney <= emp[i].money){
-                count++;
-                System.out.print(emp[i].empId + " ");
-            }
-        }
-        System.out.print("共有" + count + "人！");
-        sc.close();
-    }
+	public Employees() {
+	}
+
+	public void sift(Employees[] emp) {
+		System.out.println("窮鬼阿文要借多少錢：");
+		borrowMoney = sc.nextInt();
+		int count = 0;
+		System.out.print("員工編號：");
+		for (int i = 0; i < emp.length; i++) {
+			if (borrowMoney <= emp[i].money) {
+				count++;
+				System.out.print(emp[i].empId + " ");
+			}
+		}
+		System.out.print("共有" + count + "人！");
+		sc.close();
+	}
 }
--- src/hw4/EightPlanets.java
+++ src/hw4/EightPlanets.java
@@ -1,47 +1,42 @@
 package hw4;
 
 /**
- * ClassName： EightPlanets
- * package：hw4
- * Description：
+ * ClassName： EightPlanets package：hw4 Description：
  *
  * @Author 任少騫
  * @Create 2024/5/1 @{TIME}
  * @Version 1.0
  */
 public class EightPlanets {
-    String[] str = {"mercury","venus","earth","mars",
-            		"jupiter","saturn", "uranus","neptune"};
-    
-    char[][] c = new char[str.length][];
+	String[] str = { "mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune" };
 
-    public EightPlanets(){
+	char[][] c = new char[str.length][];
 
-    }
+	public EightPlanets() {
 
-    public void typeConversion(){
-        for (int i = 0; i < str.length; i++) {
-            c[i] = str[i].toCharArray();
-        }
-    }
+	}
 
-    public int countElements(){
-        int count = 0;
-        for (int i = 0; i < c.length; i++) {
-            for (int j = 0; j < c[i].length; j++) {
-                if(c[i][j] == 'a' || c[i][j] == 'e' || c[i][j] == 'i' ||
-                   c[i][j] == 'o' || c[i][j] == 'u'){
-                    count++;
-                }
-            }
-        }
-        return count;
-    }
+	public void typeConversion() {
+		for (int i = 0; i < str.length; i++) {
+			c[i] = str[i].toCharArray();
+		}
+	}
 
+	public int countElements() {
+		int count = 0;
+		for (int i = 0; i < c.length; i++) {
+			for (int j = 0; j < c[i].length; j++) {
+				if (c[i][j] == 'a' || c[i][j] == 'e' || c[i][j] == 'i' || c[i][j] == 'o' || c[i][j] == 'u') {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
 
-    public static void main(String[] args) {
-        EightPlanets ep = new EightPlanets();
-        ep.typeConversion();
-        System.out.println(ep.countElements());
-    }
+	public static void main(String[] args) {
+		EightPlanets ep = new EightPlanets();
+		ep.typeConversion();
+		System.out.println(ep.countElements());
+	}
 }
